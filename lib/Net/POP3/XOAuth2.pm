cd9d7ca4e69ee44081164240b1b3659a91f8c0c2
Kizashi Nagata <kizashi1122@gmail.com>
2019-08-21 12:52:05 +0900

    first implementation

--- lib/Net/POP3/XOAuth2.pm
+++ lib/Net/POP3/XOAuth2.pm
@@ -1,39 +1,19 @@
 package Net::POP3::XOAuth2;
-use 5.008001;
-use strict;
-use warnings;
 
-our $VERSION = "0.01";
+use Carp;
+use Net::POP3;
+use MIME::Base64;
 
+*Net::POP3::_AUTH = sub { shift->command('AUTH', $_[0])->response() == CMD_OK };
+*Net::POP3::xoauth2 = sub {
+    @_ >= 1 && @_ <= 3 or croak 'usage: $pop3->xoauth2( USER, TOKEN )';
+    my ($me, $user, $token) = @_;
+    my $xoauth2_token = encode_base64("user=$user\001auth=Bearer $token\001\001");
+    $xoauth2_token =~ s/[\r\n]//g;
 
+    return unless ($me->_AUTH("XOAUTH2 $xoauth2_token"));
+
+    $me->_get_mailbox_count();
+};
 
 1;
-__END__
-
-=encoding utf-8
-
-=head1 NAME
-
-Net::POP3::XOAuth2 - It's new $module
-
-=head1 SYNOPSIS
-
-    use Net::POP3::XOAuth2;
-
-=head1 DESCRIPTION
-
-Net::POP3::XOAuth2 is ...
-
-=head1 LICENSE
-
-Copyright (C) Kizashi Nagata.
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
-
-=head1 AUTHOR
-
-Kizashi Nagata E<lt>kizashi1122@gmail.comE<gt>
-
-=cut
-
