05877ff68cd90e35a1205f1ce47e4a88223adc5b
Kizashi Nagata <kizashi1122@gmail.com>
2019-08-21 12:49:30 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Net::POP3::XOAuth2 - It's new $module
+
+# SYNOPSIS
+
+    use Net::POP3::XOAuth2;
+
+# DESCRIPTION
+
+Net::POP3::XOAuth2 is ...
+
+# LICENSE
+
+Copyright (C) Kizashi Nagata.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Kizashi Nagata <kizashi1122@gmail.com>
---
+++ lib/Net/POP3/XOAuth2.pm
@@ -0,0 +1,39 @@
+package Net::POP3::XOAuth2;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Net::POP3::XOAuth2 - It's new $module
+
+=head1 SYNOPSIS
+
+    use Net::POP3::XOAuth2;
+
+=head1 DESCRIPTION
+
+Net::POP3::XOAuth2 is ...
+
+=head1 LICENSE
+
+Copyright (C) Kizashi Nagata.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Kizashi Nagata E<lt>kizashi1122@gmail.comE<gt>
+
+=cut
+
